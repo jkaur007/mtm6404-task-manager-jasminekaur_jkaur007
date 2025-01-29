477666380761152a93edc04a0e1464003228c7ad
jkaur007 <mahayjass3122@gmail.com>
2025-01-28 20:49:14 -0500

    commit

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -1,6 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './App.css';
+import Header from "./components/header";
+  
+function App() {
+  return (
+    <div className="App">
+       <Header></Header>
+    </div>
+  );
+}
+  
+export default App;
 function App() {
   return (
     <div className="App">
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,4 +14,25 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 reportWebVitals();
